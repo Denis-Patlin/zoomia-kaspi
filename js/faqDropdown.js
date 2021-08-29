a9fd9df66c36acc695b77050fb24ba4fa1278b71
Denis (desktop_home) <gunnary99@gmail.com>
2021-08-29 22:27:58 +0600

    new faq

--- js/faqDropdown.js
+++ js/faqDropdown.js
@@ -1,4 +1,27 @@
-$('.faq-heading').click(function () {
-  
-    $(this).parent('li').toggleClass('the-active').find('.faq-text').slideToggle();
+$(document).ready(function () {
+    $(".open").click(function () {
+        var container = $(this).parents(".acordion-item");
+        var trigger = container.find(".faq-t");
+        var question = container.find(".question");
+        const parent = $(this).parent();
+
+        if (parent.hasClass("acordion-item--active")) {
+            parent.removeClass("acordion-item--active");
+        } else {
+            $(".acordion-item").removeClass("acordion-item--active");
+            parent.addClass("acordion-item--active");
+        }
+        if (trigger.hasClass("faq-o")) {
+            trigger.removeClass("faq-o");
+        } else {
+            $(".faq-t").removeClass("faq-o");
+            trigger.addClass("faq-o");
+        }
+        if (question.hasClass("question-active")) {
+            question.removeClass("question-active");
+        } else {
+            $(".question").removeClass("question-active");
+            question.addClass("question-active");
+        }
+    });
 });
